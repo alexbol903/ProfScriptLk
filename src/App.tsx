60e083588d664f313172ad33732330a0def1b7d4
a.bolotin <a.bolotin@webvork.com>
2021-12-19 00:21:45 +0300

    add-app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { MainLayout } from './modules/mainLayout';
+import { Statistics } from './modules/statistics';
+
 function App() {
-  return <div></div>;
+  return (
+    <Routes>
+      <Route path="/" element={<MainLayout />}>
+        <Route path="/" element={<Navigate replace to="statistics" />} />
+        <Route path="statistics" element={<Statistics />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
